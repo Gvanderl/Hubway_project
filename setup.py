bd5f768bf2ab8922388310aef96f54943f7c1cd7
Gvanderl <gvanderl@ucsd.edu>
2018-01-13 21:43:39 -0800

    Minor changes

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name="get_hubway_data",    # This is the name of your PyPI-package.
+    version="0.1",                          # Update the version number for new releases
+    scripts=["get_hubway_data"]                  # The name of your scipt, and also the command you'll be using for calling it
+)
